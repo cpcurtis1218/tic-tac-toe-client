b23c3a676f8b6d71f71e504dbedf722b9ee5bd90
cpcurtis1218 <chuck.curtis.su@gmail.com>
2019-02-14 15:56:00 -0500

    Updates to user-message
    -Adds timer to Invalid Move update, 2 sec
    -Adds winner update

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -4,6 +4,10 @@ const ui = require('./ui.js')
 
 const alreadyClicked = function () {
   $('.user-message').html('').html('Invalid Move')
+
+  setTimeout(function () {
+    $('.user-message').html('')
+  }, 2000)
 }
 
 const resetBoard = function (turn, gameArray) {
@@ -19,35 +23,35 @@ const disableGame = function () {
 
 const checkGameOver = function (turn, gameArray) {
   if (gameArray[0] !== '' && gameArray[0] === gameArray[1] && gameArray[1] === gameArray[2]) {
-    console.log('player ' + gameArray[0] + ' is the winner!')
+    $('.user-message').html('player ' + gameArray[0] + ' is the winner!')
     disableGame()
     ui.playAgain()
   } else if (gameArray[3] !== '' && gameArray[3] === gameArray[4] && gameArray[4] === gameArray[5]) {
-    console.log('player ' + gameArray[3] + ' is the winner!')
+    $('.user-message').html('player ' + gameArray[3] + ' is the winner!')
     disableGame()
     ui.playAgain()
   } else if (gameArray[6] !== '' && gameArray[6] === gameArray[7] && gameArray[7] === gameArray[8]) {
-    console.log('player ' + gameArray[6] + ' is the winner!')
+    $('.user-message').html('player ' + gameArray[6] + ' is the winner!')
     disableGame()
     ui.playAgain()
   } else if (gameArray[0] !== '' && gameArray[0] === gameArray[3] && gameArray[3] === gameArray[6]) {
-    console.log('player ' + gameArray[0] + ' is the winner!')
+    $('.user-message').html('player ' + gameArray[0] + ' is the winner!')
     disableGame()
     ui.playAgain()
   } else if (gameArray[1] !== '' && gameArray[1] === gameArray[4] && gameArray[4] === gameArray[7]) {
-    console.log('player ' + gameArray[1] + ' is the winner!')
+    $('.user-message').html('player ' + gameArray[1] + ' is the winner!')
     disableGame()
     ui.playAgain()
   } else if (gameArray[2] !== '' && gameArray[2] === gameArray[5] && gameArray[5] === gameArray[8]) {
-    console.log('player ' + gameArray[2] + ' is the winner!')
+    $('.user-message').html('player ' + gameArray[2] + ' is the winner!')
     disableGame()
     ui.playAgain()
   } else if (gameArray[0] !== '' && gameArray[0] === gameArray[4] && gameArray[4] === gameArray[8]) {
-    console.log('player ' + gameArray[0] + ' is the winner!')
+    $('.user-message').html('player ' + gameArray[0] + ' is the winner!')
     disableGame()
     ui.playAgain()
   } else if (gameArray[2] !== '' && gameArray[2] === gameArray[4] && gameArray[4] === gameArray[6]) {
-    console.log('player ' + gameArray[2] + ' is the winner!')
+    $('.user-message').html('player ' + gameArray[2] + ' is the winner!')
     disableGame()
     ui.playAgain()
   }
