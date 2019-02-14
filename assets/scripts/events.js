6b910144176915088a1f234bcb49e3e4c929e211
cpcurtis1218 <chuck.curtis.su@gmail.com>
2019-02-14 11:07:33 -0500

    Adds disableGame function
    -Once a winner has been declared, disable all event handlers for
      game-board

--- assets/scripts/events.js
+++ assets/scripts/events.js
@@ -1,26 +1,54 @@
 'use strict'
 
-const checkGameOver = function (gameArray) {
+const ui = require('./ui.js')
+
+const disableGame = function () {
+  $('.game').off()
+}
+
+const resetBoard = function (turn, gameArray) {
+  gameArray = ['', '', '', '', '', '', '', '', '']
+  turn = 1
+  $('.game').css('background-color', 'white')
+}
+
+const checkGameOver = function (turn, gameArray) {
   if (gameArray[0] !== '' && gameArray[0] === gameArray[1] && gameArray[1] === gameArray[2]) {
     console.log('player ' + gameArray[0] + ' is the winner!')
+    disableGame()
+    ui.playAgain()
   } else if (gameArray[3] !== '' && gameArray[3] === gameArray[4] && gameArray[4] === gameArray[5]) {
     console.log('player ' + gameArray[3] + ' is the winner!')
+    disableGame()
+    ui.playAgain()
   } else if (gameArray[6] !== '' && gameArray[6] === gameArray[7] && gameArray[7] === gameArray[8]) {
     console.log('player ' + gameArray[6] + ' is the winner!')
+    disableGame()
+    ui.playAgain()
   } else if (gameArray[0] !== '' && gameArray[0] === gameArray[3] && gameArray[3] === gameArray[6]) {
     console.log('player ' + gameArray[0] + ' is the winner!')
+    disableGame()
+    ui.playAgain()
   } else if (gameArray[1] !== '' && gameArray[1] === gameArray[4] && gameArray[4] === gameArray[7]) {
     console.log('player ' + gameArray[1] + ' is the winner!')
+    disableGame()
+    ui.playAgain()
   } else if (gameArray[2] !== '' && gameArray[2] === gameArray[5] && gameArray[5] === gameArray[8]) {
     console.log('player ' + gameArray[2] + ' is the winner!')
+    disableGame()
+    ui.playAgain()
   } else if (gameArray[0] !== '' && gameArray[0] === gameArray[4] && gameArray[4] === gameArray[8]) {
     console.log('player ' + gameArray[0] + ' is the winner!')
+    disableGame()
+    ui.playAgain()
   } else if (gameArray[2] !== '' && gameArray[2] === gameArray[4] && gameArray[4] === gameArray[6]) {
     console.log('player ' + gameArray[2] + ' is the winner!')
+    disableGame()
+    ui.playAgain()
   }
 }
 
 module.exports = {
-  checkGameOver
-  // placePiece
+  checkGameOver,
+  resetBoard
 }
