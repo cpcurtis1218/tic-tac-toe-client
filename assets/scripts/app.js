d9ae5d1e1fc515878c7fcac12b9ae3304b8c49a7
cpcurtis1218 <chuck.curtis.su@gmail.com>
2019-02-13 22:35:48 -0500

    Adds currentGame array and logic
    -Adds currentGame array which tracks the current game
    -Each click will push either an x or o to the appropriate index in
      currentGame array

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -10,87 +10,123 @@
 
 $(() => {
   let currentTurn = 1
-  // each time a div is clicked, change bg color to red
+  const currentGame = ['', '', '', '', '', '', '', '', '']
+
   $('#index-0').on('click', function () {
+    // using currentTurn, determine if next click is 'o'
     if (currentTurn % 2 === 0) {
+    // turn background red
       $(this).css('background-color', 'red')
+      // assign currentGame[0] value 'o'
+      currentGame[0] = 'o'
+      // if next click is 'x'
     } else {
+      // turn background blue
       $(this).css('background-color', 'blue')
+      // assign currentGame[0] value 'x'
+      currentGame[0] = 'x'
     }
+    // increment currentTurn variable
     currentTurn++
+    // log currentGame array
+    console.log(currentGame)
+    // disable event handler for this div
     $(this).off()
   })
   $('#index-1').on('click', function () {
     if (currentTurn % 2 === 0) {
       $(this).css('background-color', 'red')
+      currentGame[1] = 'o'
     } else {
       $(this).css('background-color', 'blue')
+      currentGame[1] = 'x'
     }
     currentTurn++
+    console.log(currentGame)
     $(this).off()
   })
   $('#index-2').on('click', function () {
     if (currentTurn % 2 === 0) {
       $(this).css('background-color', 'red')
+      currentGame[2] = 'o'
     } else {
       $(this).css('background-color', 'blue')
+      currentGame[2] = 'x'
     }
     currentTurn++
+    console.log(currentGame)
     $(this).off()
   })
   $('#index-3').on('click', function () {
     if (currentTurn % 2 === 0) {
       $(this).css('background-color', 'red')
+      currentGame[3] = 'o'
     } else {
       $(this).css('background-color', 'blue')
+      currentGame[3] = 'x'
     }
     currentTurn++
+    console.log(currentGame)
     $(this).off()
   })
   $('#index-4').on('click', function () {
     if (currentTurn % 2 === 0) {
       $(this).css('background-color', 'red')
+      currentGame[4] = 'o'
     } else {
       $(this).css('background-color', 'blue')
+      currentGame[4] = 'x'
     }
     currentTurn++
+    console.log(currentGame)
     $(this).off()
   })
   $('#index-5').on('click', function () {
     if (currentTurn % 2 === 0) {
       $(this).css('background-color', 'red')
+      currentGame[5] = 'o'
     } else {
       $(this).css('background-color', 'blue')
+      currentGame[5] = 'x'
     }
     currentTurn++
+    console.log(currentGame)
     $(this).off()
   })
   $('#index-6').on('click', function () {
     if (currentTurn % 2 === 0) {
       $(this).css('background-color', 'red')
+      currentGame[6] = 'o'
     } else {
       $(this).css('background-color', 'blue')
+      currentGame[6] = 'x'
     }
     currentTurn++
+    console.log(currentGame)
     $(this).off()
   })
   $('#index-7').on('click', function () {
     if (currentTurn % 2 === 0) {
       $(this).css('background-color', 'red')
+      currentGame[7] = 'o'
     } else {
       $(this).css('background-color', 'blue')
+      currentGame[7] = 'x'
     }
     currentTurn++
+    console.log(currentGame)
     $(this).off()
   })
   $('#index-8').on('click', function () {
     if (currentTurn % 2 === 0) {
       $(this).css('background-color', 'red')
+      currentGame[8] = 'o'
     } else {
       $(this).css('background-color', 'blue')
+      currentGame[8] = 'x'
     }
     currentTurn++
+    console.log(currentGame)
     $(this).off()
   })
-  // need to track each valid click.
 })
