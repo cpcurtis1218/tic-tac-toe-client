5813624317456ef218a667818558394708cd1cdf
cpcurtis1218 <chuck.curtis.su@gmail.com>
2019-02-13 21:49:39 -0500

    Adds game logic
    -Alternating turns using counter and incrementing after each click
    -Can't click the same square more than once using .off() property

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -6,6 +6,91 @@
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
+// const events = require('./events.js')
+
 $(() => {
-  // your JS code goes here
+  let currentTurn = 1
+  // each time a div is clicked, change bg color to red
+  $('#index-0').on('click', function () {
+    if (currentTurn % 2 === 0) {
+      $(this).css('background-color', 'red')
+    } else {
+      $(this).css('background-color', 'blue')
+    }
+    currentTurn++
+    $(this).off()
+  })
+  $('#index-1').on('click', function () {
+    if (currentTurn % 2 === 0) {
+      $(this).css('background-color', 'red')
+    } else {
+      $(this).css('background-color', 'blue')
+    }
+    currentTurn++
+    $(this).off()
+  })
+  $('#index-2').on('click', function () {
+    if (currentTurn % 2 === 0) {
+      $(this).css('background-color', 'red')
+    } else {
+      $(this).css('background-color', 'blue')
+    }
+    currentTurn++
+    $(this).off()
+  })
+  $('#index-3').on('click', function () {
+    if (currentTurn % 2 === 0) {
+      $(this).css('background-color', 'red')
+    } else {
+      $(this).css('background-color', 'blue')
+    }
+    currentTurn++
+    $(this).off()
+  })
+  $('#index-4').on('click', function () {
+    if (currentTurn % 2 === 0) {
+      $(this).css('background-color', 'red')
+    } else {
+      $(this).css('background-color', 'blue')
+    }
+    currentTurn++
+    $(this).off()
+  })
+  $('#index-5').on('click', function () {
+    if (currentTurn % 2 === 0) {
+      $(this).css('background-color', 'red')
+    } else {
+      $(this).css('background-color', 'blue')
+    }
+    currentTurn++
+    $(this).off()
+  })
+  $('#index-6').on('click', function () {
+    if (currentTurn % 2 === 0) {
+      $(this).css('background-color', 'red')
+    } else {
+      $(this).css('background-color', 'blue')
+    }
+    currentTurn++
+    $(this).off()
+  })
+  $('#index-7').on('click', function () {
+    if (currentTurn % 2 === 0) {
+      $(this).css('background-color', 'red')
+    } else {
+      $(this).css('background-color', 'blue')
+    }
+    currentTurn++
+    $(this).off()
+  })
+  $('#index-8').on('click', function () {
+    if (currentTurn % 2 === 0) {
+      $(this).css('background-color', 'red')
+    } else {
+      $(this).css('background-color', 'blue')
+    }
+    currentTurn++
+    $(this).off()
+  })
+  // need to track each valid click.
 })
