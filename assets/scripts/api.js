e60c551874331a307d83cee9470ed7d8c87df4de
cpcurtis1218 <chuck.curtis.su@gmail.com>
2019-02-15 09:45:28 -0500

    Adds api.js file with working sign-up

---
+++ assets/scripts/api.js
@@ -0,0 +1,15 @@
+'use strict'
+
+const config = require('./config.js')
+
+const signUp = (formData) => {
+  return $.ajax({
+    url: config.apiUrl + '/sign-up',
+    method: 'POST',
+    data: formData
+  })
+}
+
+module.exports = {
+  signUp
+}
